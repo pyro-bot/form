d0d45400df1f8fa840cf28ae2007eefdeb241736
Shicselov A <pyro-bot@yandex.ru>
2019-04-04 16:28:00 +0500

    fix disabled button

--- form/static/js/main.js
+++ form/static/js/main.js
@@ -4,4 +4,9 @@ $(document).ready(function(){
         $("#mybutton").prop({"disabled":false});
     });
 
+    $('textarea').on('blur', function (e) {
+        if (e.relatedTarget === null || e.relatedTarget.id !== 'mybutton')
+            $('#mybutton').prop({'disabled': true})
+    })
+
 });
